d9bef1b45603cb65304bbcb4d8978d9ac061289d
shailendra28 <yshailendrakumar1768@gmail.com>
2021-07-30 22:59:42 +0530

    Added saving the form data after clicking the submit button

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -20,3 +20,55 @@ salary.addEventListener('input', function() {
     output.textContent = salary.value;
 }); 
 });
+const save = () => {
+    try {
+        let employeePayrollData = createEmployeePayroll();
+    }catch (e) {
+        return;
+    }
+}
+const createEmployeePayroll = () => {
+  let employeePayrollData = new EmployeePayrollData();
+  try{
+    employeePayrollData.name = getInputValueById('#name');
+  }catch (e){
+    setTextValue('.text-error',e);
+    throw e;
+  }
+  employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+  employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+  employeePayrollData.departement = getSelectedValues('[name=departement]');
+  employeePayrollData.salary = getSelectedValues('#salary');
+  employeePayrollData.note = getSelectedValues('#note');
+  let date = getInputValueById('#day')+" "+getInputValueById('#month')+" "+
+             getInputValueById('#year');
+  employeePayrollData.date = Date.parse(date);
+  alert(employeePayrollData.toString());
+  return employeePayrollData;           
+}
+const getSelectedValues = (propertyValue) => {
+  let allItems = document.querySelectorAll(propertyValue);
+  let selItems = [];
+  allItems.forEach(item => {
+    if(item.checked) selItems.push(item.value); 
+  });
+  return selItems;
+}
+/*
+*1: querySelector is  the newer feature.
+*2: The querySelector  method  can be used when selecting by element name,nesting, or class name.
+*3: querySelector lets you find elements with rules that can't be expressed with getElementById
+*/
+const  getInputValueById = (id) => {
+  let value = document.querySelector(id).value;
+  return value;
+}
+
+/*
+* 1: getElementById is better supported than querySelector in the older version of the browsers.
+* 2: The thing with getElementById is that it only allows to select an element by its id. 
+*/
+const getElementValue = (id) => {
+  let value = document.getElementById(id).value;
+  return value;
+}
